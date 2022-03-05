aea975a68b4bba6bb4ba10616d1bb0a493477546
v24kuon <63438234+v24kuon@users.noreply.github.com>
2022-03-05 20:12:27 +0900

    useEffect勉強内容2

--- src/UseEffect1.tsx
+++ src/UseEffect1.tsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react';
 
 const UseEffect1 = () => {
   const [count, setCount] = useState<number>(0);
+  const [item, setItem] = useState<string>('');
   useEffect(() => {
     console.log('useEffectだよ');
-  }, []);
+  }, [count]);
   return (
     <div>
       <button onClick={() => setCount((pre) => pre + 1)}>{count}</button>
+      <input type='text' value={item} onChange={(e) => setItem(e.target.value)} />
     </div>
   );
 };
